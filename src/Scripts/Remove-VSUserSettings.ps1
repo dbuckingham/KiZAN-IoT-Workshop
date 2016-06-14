938313924bf354e3f5c16290ac9092f4e47735cf
David Buckingham <david.buckingham@outlook.com>
2016-06-14 14:59:09 -0400

    created Remove-VSUserSettings script

---
+++ src/Scripts/Remove-VSUserSettings.ps1
@@ -0,0 +1,5 @@
+﻿# Find the root of the repository.
+$root = git rev-parse --show-toplevel
+
+# Remove Visual Studio user project option files.
+Get-ChildItem -Path $root\src -Filter *.user -Recurse | Remove-Item
